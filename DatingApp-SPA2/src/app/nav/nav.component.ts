69a812d1f7ce874fc90ec87107b65bdebd0869d3
unknown <same.dan@gmail.com>
2018-09-21 08:55:07 +0200

    End od Section 4

---
+++ DatingApp-SPA2/src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in succesfully');
+    }, error => {
+      console.log('Failed to log in');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    // is there's something in the token, return 'true', else return 'false'
+      return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
+}
